1df1832a06857ea13d631ba57bc5aaee851679df
lazyBoy <lazyBoy@ail.com>
2022-04-09 22:39:17 +0200

    all files added

---
+++ script.js
@@ -0,0 +1,21 @@
+let misol = +prompt('1 + 1 + 1 + 1 +2 * 2 + 1 + 1 + 1 + 1')
+let misol2 = +prompt('3 * 3 * 3 - 3 * 2 * 3')
+let misol3 = +prompt('1 + 1 + 1 - 1 * 2 * 1') 
+let misol4 = +prompt('112 / 3 + 127')
+let misol5 = +prompt('144 / 12 / 6')
+ 
+console.log('1 + 1 + 1 + 1 +2 * 2 + 1 + 1 + 1 + 1');
+console.log('Sizning javobingiz '+ misol);
+console.log("To'gri javob 12");
+console.log('3 * 3 * 3 - 3 * 2 * 3');
+console.log('Sizning javobingiz ' + misol2);
+console.log("To'gri javob 9");
+console.log('1 + 1 + 1 - 1 * 2 * 1');
+console.log('Sizning javobingiz ' + misol3);
+console.log("To'gri javob 1");
+console.log('112 / 3 + 127');
+console.log('Sizning javobingiz ' + misol4);
+console.log("To'gri javob 161");
+console.log('144 / 12 / 6');
+console.log('Sizning javobingiz ' + misol5);
+console.log("To'gri javob 2");
